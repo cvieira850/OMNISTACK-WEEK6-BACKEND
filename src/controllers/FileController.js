7385afa9269ef3814b89ef2fc9e56e809dadc121
caio vieira <caiodmvieiragithub@gmail.com>
2019-04-09 23:10:57 -0300

    criação da url para acessar o arquivo enviado

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -1,11 +1,20 @@
-const File = require("../models/Box");
+const Box = require("../models/Box");
+const File = require("../models/File");
 
 class FileController {
   async store(req, res) {
-    // Criar um arquivo
-    console.log(req.file);
+    const box = await Box.findById(req.params.id);
 
-    return res.send("OK");
+    const file = await File.create({
+      title: req.file.originalname,
+      path: req.file.key
+    });
+
+    box.files.push(file);
+
+    await box.save();
+
+    return res.json(file);
   }
 }
 
--- src/models/File.js
+++ src/models/File.js
@@ -9,12 +9,18 @@ const File = new mongoose.Schema(
     path: {
       type: String,
       required: true
-    },
-    files: []
+    }
   },
   {
-    timestamps: true
+    timestamps: true,
+    //toda vez que o arquivo for transformado em JSON ou objeto ele chama file.virtual automaticamente
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
   }
 );
+
+File.virtual("url").get(function() {
+  return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
+});
 
 module.exports = mongoose.model("File", File);
